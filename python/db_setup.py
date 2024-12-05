4640ffcd25b8f8550ac6dfb5f80e85be26897001
Manami63 <manami6300@gmail.com>
2024-12-05 16:24:17 +0900

    add test data

--- python/db_setup.py
+++ python/db_setup.py
@@ -10,7 +10,18 @@ def make_testData_person(conn: sqlite3.Connection) -> None:
         ('eccbc87e4b5ce2fe28308fd9f2a7baf3','田中氏', 1, 22, 0, 'Lactose intolerance', 'Has pets'), # 3
         ('a87ff679a2f3e71d9181a67b7542122c','チャンさく', 2, 28, 0, '', ''), # 4
         ('e4da3b7fbbce2345d7772b0674a318d5','みかきん', 1, 30, 1, 'Gluten allergy, Prefers vegetarian', ''), # 5
-        ('1679091c5a880faf6fb5e6087eb1b2dc','さよねぇ', 2, 28, 0, '', '') # 6
+        ('1679091c5a880faf6fb5e6087eb1b2dc','さよねぇ', 2, 28, 0, '', ''), # 6
+        ('8f14e45fceea167a5a36dedd4bea2543', 'もっちゃん', 2, 31, 0, '', ''), # 7
+        ('c9f0f895fb98ab9159f51fd0297e236d','さよ', 2, 50, 0, '', ''), # 8
+        ('45c48cce2e2d7fbdea1afc51c7c6ad26', 'しゅんぺい', 3, 29, 0, '', ''), # 9
+        ('d3d9446802a44259755d38e6d163e820', 'はるちゃん', 3, 22, 1, 'ピーナッツ', '喘息持ち'), # 10
+        ('6512bd43d9caa6e02c990b0a82652dca', 'まーくん', 3, 28, 0, '', ''), # 11
+        ('c20ad4d76fe97759aa27a0c99bff6710', '福山', 3, 32, 1, 'カニ', ''), # 12
+        ('c51ce410c124a10e0db5e4b97fc2af39', 'ちかちゃん', 4, 26, 0, '', ''), # 13
+        ('aab3238922bcc25a6f606eb525ffdc56', 'こうた', 4, 24, 1, '乳製品', ''), # 14
+        ('9bf31c7ff062936a96d3c8bd1f8f2ff3', 'さくら', 4, 29, 0, '', ''), # 15
+        ('c74d97b01eae257e44aa9d5bade97baf', 'ゆかりん', 4, 27, 1, 'そば', 'アレルギーは軽度') # 16
+
     ])
     conn.commit()
 
@@ -26,6 +37,9 @@ def make_testData_serveLog(conn: sqlite3.Connection) -> None:
         ('c81e728d9d4c2f636f067f89cc14862c', False, 3),
         ('c81e728d9d4c2f636f067f89cc14862c', False, 1),
         ('e4da3b7fbbce2345d7772b0674a318d5', True, 2)
+        ('e4da3b7fbbce2345d7772b0674a318d5', True, 2)
+        ('c74d97b01eae257e44aa9d5bade97baf', True, 3)
+        ('c74d97b01eae257e44aa9d5bade97baf', True, 5)
     ])
     conn.commit()
 
@@ -38,11 +52,14 @@ def make_testData_stockList(conn: sqlite3.Connection) -> None:
         ('水', '250', 'ml', 0, 1),              # 2
         ('水', '500', 'ml', 0, 1),              # 3
         ('水', '1000', 'ml', 0, 1),             # 4
-        ('クルミパン', None, '個', 1, 1),         # 5
-        ('食パン', None, '斤', 24, 1),           # 6
-        ('乾電池', '単4', '本', 0, 2),           # 7
-        ('乾電池', '単3', '本', 0, 2),           # 8
-        ('絆創膏', None, '枚', 0, 3)            # 9
+        ('クルミパン', None, '個', 1, 1),        # 5
+        ('缶パン', None, '個', 24, 1),           # 6
+        ('クラッカー', None, '個', 24, 1),       # 7        ('みそ汁', None, '杯', 24, 1),           # 6
+        ('おにぎり', None, '個', 24, 1),         # 8
+        ('おかゆ', None, '個', 24, 1),           # 9
+        ('乾電池', '単4', '本', 0, 2),           # 10
+        ('乾電池', '単3', '本', 0, 2),           # 11
+        ('絆創膏', None, '枚', 0, 3)            # 12
     ])
     conn.commit()
 

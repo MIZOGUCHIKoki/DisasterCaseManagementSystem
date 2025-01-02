dfd5c57d2d9bce7752977f5fd4f749dd32baef89
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2025-01-03 02:01:30 +0900

    Update

--- python/test/test_RW/conftest.py
+++ python/test/test_RW/conftest.py
@@ -8,19 +8,11 @@ from db_setup import setup_db_person
 
 @pytest.fixture(scope="module")
 async def setup_db():
-    database = Database("sqlite:///dataBase.db")
-    await database.connect()
-    conn = sqlite3.connect(database)
-    setup_db_person(conn)
-    print("Test database connected")
 
-    yield
-
-    conn.close()
-    await database.disconnect()
-    print("Test database disconnected")
+    yield 
 
 @pytest.fixture(scope="module")
 def client():
     client = TestClient(app)
     yield client
+
